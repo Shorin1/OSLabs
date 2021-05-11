09d3b12c8075f7158dbd24b0271b8d0195cc6a92
Dmitriy <balalajkalodocnik@gmail.com>
2021-05-11 22:16:53 +0400

    Переделал на запись-чтение в отдельных потоках и синхронизацию их через семафоры

--- Lab4WinForm/Form1.cs
+++ Lab4WinForm/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab4WinForm
@@ -9,50 +8,105 @@ namespace Lab4WinForm
     public partial class Form1 : Form
     {
         public delegate void InvokeDelegate();
-        private readonly object syncObj;
+
+        /// <summary>
+        /// Семафор для потока, в котором происходит чтение-запись
+        /// </summary>
+        private readonly Semaphore buttonSemaphore;
+
+        /// <summary>
+        /// Семафор для запуска потоков, чтобы чтение не начиналось раньше записи
+        /// </summary>
+        private readonly Semaphore startThreadsSemaphore;
+
+        /// <summary>
+        /// Семафор на чтение-запись. Пока происходит чтение запись невозможна
+        /// </summary>
+        private readonly Semaphore readWriteSemaphore;
+
+        /// <summary>
+        /// Флаг означающий конец текста для записи
+        /// </summary>
+        private bool isEnd;
 
         public Form1()
         {
-            syncObj = new object();
+            isEnd = false;
+            buttonSemaphore = new Semaphore(1, 1);
+            startThreadsSemaphore = new Semaphore(1, 1);
+            readWriteSemaphore = new Semaphore(1, 1);
             InitializeComponent();
         }
 
         private void runButton_Click(object sender, EventArgs e)
         {
+            var readWriteThread = new Thread(new ThreadStart(() => ReadWrite()));
+            readWriteThread.Start();
+        }
+
+        /// <summary>
+        /// Чтение-запись в отдельном потоке (фоне)
+        /// </summary>
+        private void ReadWrite()
+        {
             int.TryParse(speedTextBox.Text, out int delay);
-            ReadWriteAsync(delay);
+
+            var fileName = "text.txt";
+            var text = inputTextBox.Text;
+            
+            // Блокируем чтоб новые операции на чтение-запись ждали пока закончатся предыдущие
+            buttonSemaphore.WaitOne();
+
+            var writeThread = new Thread(new ThreadStart(() => Write(fileName, text, delay)));
+            writeThread.Start();
+
+            // Блокируем чтоб чтения не начиналось раньше записи
+            startThreadsSemaphore.WaitOne();
+
+            var readThread = new Thread(new ThreadStart(() => Read(fileName)));
+            readThread.Start();
         }
 
-        private void ReadWriteAsync(int delay)
+        private void Write(string fileName, string text, int delay)
         {
-            var thread = new Thread(new ThreadStart(() => ReadWrite(delay)));
-            thread.Start();
-        }
-
-        private void ReadWrite(int delay)
-        {
-            lock (syncObj)
+            foreach (var letter in text)
             {
-                var fileName = "file.txt";
-                var text = inputTextBox.Text;
-
-                foreach (var letter in text)
+                // Блокируем поток чтобы чтение не начиналось пока не закончилась запись
+                readWriteSemaphore.WaitOne();
+                using (var sw = new StreamWriter(fileName, true))
                 {
-                    using (var sw = new StreamWriter(fileName, true))
-                    {
-                        sw.Write(letter);
-                    }
-
-                    Thread.Sleep(delay);
-
-                    using (var sr = new StreamReader(fileName))
-                    {
-                        var lines = sr.ReadToEnd();
-                        outputTextBox.BeginInvoke(new InvokeDelegate(() => outputTextBox.Text = lines));
-
-                    }
+                    sw.Write(letter);
                 }
+                Thread.Sleep(delay);
+                // Высвобождаем семафор чтения-записи
+                readWriteSemaphore.Release();
+                // Высвобождаем семафор синхранизации старта задач 
+                startThreadsSemaphore.Release();
             }
+
+            isEnd = true;
+        }
+
+        private void Read(string fileName)
+        {
+            while (!isEnd)
+            {
+                // Ждем окончания блокировки семафора синхронизации старта задач
+                startThreadsSemaphore.WaitOne();
+                // Ждем окончания блокировки семафора синхронизации чтения-записи
+                readWriteSemaphore.WaitOne();
+                using (var sr = new StreamReader(fileName))
+                {
+                    var text = sr.ReadToEnd();
+                    // ui поток не позволяет обращаться к нему из других потоков поэтому юзаем такой костыль
+                    outputTextBox.BeginInvoke(new InvokeDelegate(() => outputTextBox.Text = text));
+                }
+                // Высвобождаем семафор чтения-записи
+                readWriteSemaphore.Release();
+            }
+            
+            // Высвобождаем семафор потока в котором начинаются операции чтения-записи
+            buttonSemaphore.Release();
         }
     }
 }
