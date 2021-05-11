d83cbc5c252ab5ec33b5a446ced37305a5e7dc7f
Dmitriy <balalajkalodocnik@gmail.com>
2021-05-11 20:32:56 +0400

    4 лаба

---
+++ Lab4WinForm/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Lab4WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.speedTextBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(13, 13);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(350, 200);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Enabled = false;
+            this.outputTextBox.Location = new System.Drawing.Point(13, 219);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(350, 200);
+            this.outputTextBox.TabIndex = 1;
+            // 
+            // speedTextBox
+            // 
+            this.speedTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.speedTextBox.Location = new System.Drawing.Point(369, 31);
+            this.speedTextBox.Name = "speedTextBox";
+            this.speedTextBox.Size = new System.Drawing.Size(97, 23);
+            this.speedTextBox.TabIndex = 2;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(472, 31);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 3;
+            this.runButton.Text = "Начать";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(369, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Скорость";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 429);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.speedTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.TextBox speedTextBox;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab4WinForm/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4WinForm
+{
+    public partial class Form1 : Form
+    {
+        public delegate void InvokeDelegate();
+        private readonly object syncObj;
+
+        public Form1()
+        {
+            syncObj = new object();
+            InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            int.TryParse(speedTextBox.Text, out int delay);
+            ReadWriteAsync(delay);
+        }
+
+        private void ReadWriteAsync(int delay)
+        {
+            var thread = new Thread(new ThreadStart(() => ReadWrite(delay)));
+            thread.Start();
+        }
+
+        private void ReadWrite(int delay)
+        {
+            lock (syncObj)
+            {
+                var fileName = "file.txt";
+                var text = inputTextBox.Text;
+
+                foreach (var letter in text)
+                {
+                    using (var sw = new StreamWriter(fileName, true))
+                    {
+                        sw.Write(letter);
+                    }
+
+                    Thread.Sleep(delay);
+
+                    using (var sr = new StreamReader(fileName))
+                    {
+                        var lines = sr.ReadToEnd();
+                        outputTextBox.BeginInvoke(new InvokeDelegate(() => outputTextBox.Text = lines));
+
+                    }
+                }
+            }
+        }
+    }
+}
